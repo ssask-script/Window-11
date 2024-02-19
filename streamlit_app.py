57805b4aad8dd4b7357429846761059cc96821fd
Ssask <122962997+ssask-script@users.noreply.github.com>
2024-02-19 11:46:02 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
+import streamlit as st
 import http.server
 import socketserver
 
-PORT = 8000  # You can change this to any port you prefer
+
+PORT = 8501
 
 Handler = http.server.SimpleHTTPRequestHandler
+
+# Change to the directory where your index.html file is located
+web_dir = "."
+os.chdir(web_dir)
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("Serving at port", PORT)
